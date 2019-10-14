80c8af8994bb861019679676f4f677dba5293894
dankennedy97 <danielk5275@yahoo.ie>
2019-10-14 16:33:13 +0100

    Made sure king can only move 1 space at a time

--- ChessProject.java
+++ ChessProject.java
@@ -242,6 +242,16 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
             completing the move...
            }
           */
+          //to ensure the king can only move one space
+          if(((startY - landingY == 1)&&(startX - landingX == 0))||//allows king to move forward, based on orientation
+          (startY - landingY == -1)&&(startX - landingX == 0)||//allows backwards movement, based on orientation
+          (startY - landingY == 0)&&(startX - landingX == 1)||//allows left movement, based on orientation
+          (startY - landingY == 0)&&(startX - landingX == -1)||//allows right movement, based on orientation
+          (startX - landingX == -1)&&(startY - landingY == -1)||//allows diagonal movement, based on orientation
+          (startX - landingX == 1)&&(startY - landingY == 1)||// allows diagonal movement, based on orientation
+          (startX - landingX == 1)&&(startY -landingY == -1)||// allows diagonal movement, based on orientation
+          (startX - landingX == -1)&&(startY - landingY == 1))// allows diagonal movement, based on orientation
+          {
           // Diagonal Movement for King
             if (Math.abs(startX - landingX) == Math.abs(startY - landingY)) {
               Boolean inTheWay = false;
@@ -395,6 +405,7 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
               }
             }
           }
+        }
         else if(pieceName.contains("Bishop")){
           Boolean inTheWay = false;
           int distance = Math.abs(startX-landingX);
